a4114e375c8aac4a4beb83fca3024a4dfb1db07e
Heba Elsaid <hebaelsaid912@gmail.com>
2022-12-26 14:25:54 +0200

    handle splash screen design and logic

--- app/src/main/java/com/hebaelsaid/android/storeapp/ui/feature/splash/SplashFragment.kt
+++ app/src/main/java/com/hebaelsaid/android/storeapp/ui/feature/splash/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.hebaelsaid.android.storeapp.ui.feature.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.hebaelsaid.android.storeapp.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
@@ -24,7 +27,9 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToProductListFragment())
+        }, 3000)
     }
 
 }
